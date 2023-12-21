eba015b84fea68126cdd28e183606ab114ace07d
Ezzaldin97 <kingtal632@yahoo.com>
2023-12-21 11:47:52 +0200

    UPDATE: remove geocoders temporarily

--- app/api_data/weather_data_flows.py
+++ app/api_data/weather_data_flows.py
@@ -1,7 +1,7 @@
 from prefect import task, flow
 from prefect.tasks import task_input_hash
 from app.api_data.data_models import URLParams, APIData
-from geopy.geocoders import Nominatim
+#from geopy.geocoders import Nominatim
 import requests
 import duckdb
 import pandas as pd
@@ -78,13 +78,13 @@ def transform_api_data(data: APIData) -> pd.DataFrame:
     """
     print("Transform Data to Database Schema like")
     print("Get Location Data of API Data")
-    lat, long = data["latitude"], data["longitude"]
-    app = Nominatim(user_agent="ml_batch_job")
-    coordinates = f"{lat}, {long}"
-    location_data = app.reverse(coordinates, language="en").raw
+    #lat, long = data["latitude"], data["longitude"]
+    #app = Nominatim(user_agent="ml_batch_job")
+    #coordinates = f"{lat}, {long}"
+    #location_data = app.reverse(coordinates, language="en").raw
     df = pd.DataFrame({
         "location_id": [
-            location_data["place_id"] for _ in range(len(data["hourly"]["time"]))
+            75354428 for _ in range(len(data["hourly"]["time"]))
         ],
         "reading_timestamp": data["hourly"]["time"],
         "temprature": data["hourly"]["temperature_2m"],
